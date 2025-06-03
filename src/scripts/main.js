7eedc5dc2f5e94beb99d888047902e4f348251fa
Aweiro <arsenskott@gmail.com>
2025-06-03 22:44:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const span = [...document.getElementsByClassName('population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const newArr = span.map((el) => +el.innerHTML.split(',').join(''));
+
+const sum = newArr.reduce((text, text2) => text + text2, 0);
+const average = sum / newArr.length;
+
+function formatKey(key) {
+  const editKey = String(key);
+  const firstPart = editKey.length % 3;
+  let result = firstPart > 0 ? editKey.slice(0, firstPart) + ',' : '';
+
+  for (let i = firstPart; i < editKey.length; i += 3) {
+    result += editKey.slice(i, i + 3) + ',';
+  }
+
+  return result.slice(0, result.length - 1);
+}
+
+totalPopulation.innerHTML = formatKey(sum);
+averagePopulation.innerHTML = formatKey(Math.round(average));
